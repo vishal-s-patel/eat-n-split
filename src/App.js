433e5384358549cbd3712c806b28fb54d845ccf9
vishal-s-patel <vishalpatel1002@yahoo.com>
2024-07-30 22:49:29 +0530

    fix: state reset

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ export default function App() {
         <FormSplitBill
           friendName={selectedFriend.name}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
